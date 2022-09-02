b8713de8bcb00ce06a39f0976052ca32940b6a32
oktayafandi <ogtayaa@code.edu.az>
2022-09-02 10:52:52 +0400

    pagination and total cound done for api tokens page

--- src/api/Token.ts
+++ src/api/Token.ts
@@ -1,10 +1,16 @@
-import IToken from "../types/Token";
+import IToken, {IFilter} from "../types/Token";
 import { AxiosResponse } from "axios";
 import api from "../config/ApiConfig";
 
-export const getTokens = (): Promise<IToken[]> => {
-  return api.get("/apitokens" ).then((res: AxiosResponse) => res.data);
+export const getTokens = ({page, limit = 10}: IFilter): Promise<{count: number, data: IToken[]}> => {
+  return api.get("/apitokens", {
+    params: {
+      _page: page,
+      _limit: limit
+    }
+  }).then((res: AxiosResponse) => ({data: res.data,count: parseInt(res.headers['x-total-count']) }));
 };
+
 
 export const postTokens = (data: IToken[]) => {
   return api.post("/apitokens", data).then((res: AxiosResponse) => res.data);
--- src/store/TokenStore.ts
+++ src/store/TokenStore.ts
@@ -1,20 +1,35 @@
 import { observable, action, makeAutoObservable, reaction } from "mobx";
 import { getTokens, postTokens } from "../api/Token";
-import IToken from "../types/Token";
+import IToken, { IFilter } from "../types/Token";
 
 export class TokensStore {
   @observable tokens: IToken[] = [];
-
+  @observable datas: IToken[] = [];
+  @observable count: number = 0;
+  @observable isloading: boolean = false;
+  @observable filter: IFilter = {
+    page: 1,
+    limit: 10,
+  };
+  
   constructor() {
     makeAutoObservable(this);
+    reaction(() => [this.filter], () => this.loadTokens())
   }
 
   @action
   loadTokens = () => {
-    getTokens().then((tokens) => {
-      this.tokens = tokens;
-    });
+    this.isloading = true;
+    getTokens(this.filter).then(({data, count}) => {
+      this.tokens = data;
+      this.count = count;
+    }).finally(() => (this.isloading = false));
   };
+  
+  @action
+  setFilter(filter: Partial<IFilter>) {
+    this.filter = { ...this.filter, ...filter };
+  }
 
   @action
   saveTokens = () => {
