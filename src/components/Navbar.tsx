c9256027d40f411c89d5c9d3e26bcb63894790dc
Tural <tural.ismayil0981@gmail.com>
2022-09-02 12:05:58 +0400

    c

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,45 @@
-import { PageHeader, Menu, Dropdown, Avatar } from "antd";
-import { UserOutlined } from "@ant-design/icons";
-import { Link, useLocation } from "react-router-dom";
+import { PageHeader, Menu, Dropdown, Avatar, Space } from "antd";
+import { DownOutlined, UserOutlined } from "@ant-design/icons";
+import { Link, Navigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default () => {
-  const loc = useLocation()
+  const loc = useLocation();
+  const navigate = useNavigate();
+
+  const items = [
+    { label: <Link to="/">Home</Link>, key: "/" },
+    { label: <Link to="/users">Users</Link>, key: "/users" },
+    { label: <Link to="/transactions">Transactions</Link>, key: "/transactions"},
+    { label: <Link to="/tokens">API Tokens</Link>, key: "/tokens" },
+  ];
 
   const menu = (
-    <Menu>
-      <Menu.Item>
-        <a target="_blank" rel="noopener noreferrer">
-          Çıxış et
-        </a>
-      </Menu.Item>
-    </Menu>
+    <Menu
+      items={[
+        {
+          label: (
+            <Link target="_blank"  to="/details" >
+              Profil
+            </Link>
+          ),
+          key: '0',
+        },
+        {
+          label: (
+            <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
+              Cixis
+            </a>
+          ),
+          key: '1',
+        },
+        {
+          type: 'divider',
+        },
+      ]}
+    />
   );
+  
 
   return (
     <div className="site-page-header-ghost-wrapper">
@@ -22,21 +48,23 @@ export default () => {
         title={"PAS"}
         extra={[
           <div className="nav-buttons">
-            <Dropdown overlay={menu} placement="bottomCenter">
-              <a className="profile-button">
-                Oktay Afandi
-                <Avatar icon={<UserOutlined />} />
-              </a>
-            </Dropdown>
-          </div>
+            <Dropdown overlay={menu}>
+                <a onClick={e => e.preventDefault()}>
+                  <Space>
+                     Oktay Afandi
+                    <DownOutlined />
+                  </Space>
+                  <Avatar icon={<UserOutlined />} />
+                </a>
+              </Dropdown>
+          </div>,
         ]}
       />
-      <Menu mode="horizontal" selectedKeys={[loc.pathname]}>
-        <Menu.Item key="/"><Link to='/'>Home</Link></Menu.Item>
-        <Menu.Item key="/users"><Link to='/users'>Users</Link></Menu.Item>
-        <Menu.Item key="/transactions"><Link to='/transactions'>Transactions</Link></Menu.Item>
-        <Menu.Item key="/tokens"><Link to='/tokens'>API Tokens</Link></Menu.Item>
-      </Menu>
+      <Menu
+        mode="horizontal"
+        selectedKeys={[loc.pathname]}
+        items={items}
+      ></Menu>
     </div>
   );
 };
